d7d84aefbbf736c0e9fc525a85ba47268becb6cd
[Florabel Ituralde] <67927764+florabelituralde@users.noreply.github.com>
2020-10-21 19:03:04 -0400

    Updates

--- Medikeeper_Project/Models/Store.cs
+++ Medikeeper_Project/Models/Store.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,8 +10,13 @@ namespace Medikeeper_Project.Models
     public class Store
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Cost { get; set; }
     }
--- Medikeeper_Project/obj/Release/netcoreapp3.1/Medikeeper_Project.AssemblyInfo.cs
+++ Medikeeper_Project/obj/Release/netcoreapp3.1/Medikeeper_Project.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("8f5118b3-c59b-48df-a42f-29d2abf371f1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Medikeeper_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
