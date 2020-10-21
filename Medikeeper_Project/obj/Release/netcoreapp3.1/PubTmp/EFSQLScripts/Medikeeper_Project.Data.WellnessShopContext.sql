0c1d598b08995a319e47a74bce01b73a70487309
[Florabel Ituralde] <67927764+florabelituralde@users.noreply.github.com>
2020-10-21 17:30:36 -0400

    tests

---
+++ Medikeeper_Project/obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/Medikeeper_Project.Data.WellnessShopContext.sql
@@ -0,0 +1,31 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20201017231219_InitialCreate')
+BEGIN
+    CREATE TABLE [Store] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        [Cost] decimal(18, 2) NOT NULL,
+        CONSTRAINT [PK_Store] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20201017231219_InitialCreate')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20201017231219_InitialCreate', N'3.1.9');
+END;
+
+GO
+
